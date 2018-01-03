d015efa9d346afd67fd0d1a30ba1a9b3add21e38
Murat Utku Keti <veyluna@outlook.com>
2018-01-03 19:18:36 +0300

    Added firing and movement

---
+++ Assets/Scripts/NinjaGirl/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    Rigidbody2D rigidbody;
+
+    public float speed = 20f;
+    int direction = 1;
+
+    public float lifetime = 2f;
+
+    public int damage = 5;
+
+	void Start () {
+        rigidbody = transform.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+        rigidbody.velocity = new Vector2(direction * speed, rigidbody.velocity.y);
+
+        lifetime -= Time.deltaTime;
+
+        if (lifetime <= 0)
+            Destroy(transform.gameObject);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            print("damaged");
+            collision.gameObject.SendMessage("Damage", damage, SendMessageOptions.DontRequireReceiver);
+            Destroy(transform.gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/NinjaGirl/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    Rigidbody2D rigidbody;
+
+    public GameObject bullet;       //Ateş edilecek mermi ve çıkış noktası
+    public Transform attackLoc;
+
+    public float maxSpeed = 5f;     //Koşma için olan maxSpeed
+
+    bool b_goright = false;         //Sağa sola gitmeyi kontrol eden bollar
+    bool b_goleft = false ;
+
+	void Start () {
+        rigidbody = transform.gameObject.GetComponent<Rigidbody2D>();
+
+    }
+	
+	void FixedUpdate () {
+		if(b_goright)
+            rigidbody.velocity = new Vector2(1 * maxSpeed, rigidbody.velocity.y);       //Sağa gidiş
+
+        if(b_goleft)
+            rigidbody.velocity = new Vector2(-1 * maxSpeed, rigidbody.velocity.y);      //Sola gidiş
+    }
+
+    public void GoRight()           //Sağa giderken olması gerenler
+    {
+        b_goright = true;
+    }
+
+    public void GoLeft()            //Sola giderken olması gerekenler
+    {
+        b_goleft = true;
+    }
+
+    public void PointerUp()
+    {
+        rigidbody.velocity = new Vector3(0, rigidbody.velocity.y);         //Hiçbir tuşa basmıyorken durmasını sağlar
+        b_goleft = false;
+        b_goright = false;
+    }
+
+    public void Attack()
+    {
+        Invoke("InvokeBullet", 0.0f);       //Delayle mermi atımını sağlar
+    }
+
+    public void InvokeBullet()
+    {
+        Instantiate(bullet, attackLoc.position, attackLoc.rotation);    //Mermi oluşturur
+    }
+}
